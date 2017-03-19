b44e23334dc5ced73a95c581b79b415ef6b3c3b3
Ioannis Psarros <ipsarros@di.uoa.gr>
2017-03-19 12:06:36 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -3,18 +3,18 @@ sys.path.append('/usr/local/lib/python3.4/dist-packages/')
 import numpy as np
 
 
-def fvecs_read(filename, c_contiguous=True):
+def fvecs_read(filename):
     fv = np.fromfile(filename, dtype=np.float32)
     if fv.size == 0:
-        return np.zeros((0, 0))
+         raise IOError("Empty dataset in " + filename)
     dim = fv.view(np.int32)[0]
-    assert dim > 0
-    fv = fv.reshape(-1, 1 + dim)
+    if dim <= 0:
+        raise IOError("Dimension <=0 in " + filename)
+    fv = fv.reshape(-1, 1 + dim) 
     if not all(fv.view(np.int32)[:, 0] == dim):
         raise IOError("Non-uniform vector sizes in " + filename)
     fv = fv[:, 1:]
-    if c_contiguous:
-        fv = fv.copy()
+    fv = fv.copy()
     return (dim,fv)
 def findmean(P, D, r):
     if len(P)>1000:
